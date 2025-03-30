b73d733ca7816667e41cdd3d3c787835e6fc936a
Ken Lasko <ken.lasko@gmail.com>
2025-03-30 19:39:13 +0000

    Update kubectl provider version across multiple modules

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "= 2.36.0"
     }
     helm = {
       source = "hashicorp/helm"
+      version = "= 2.17.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+      version = "1.19.0"
     }
   }
 }
