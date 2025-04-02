b2b4c516456efc681234d178ee5bc89d859c1a54
Ken Lasko <ken.lasko@gmail.com>
2025-04-02 10:36:24 -0400

    Add Gateway API CRD support to Cilium module

--- modules/cilium/main.tf
+++ modules/cilium/main.tf
@@ -27,7 +27,22 @@ variable "manifests_dir" {
 variable "kube_context" {
   type = string
 }
+variable "gateway_api_crd_version" {
+  type = string
+  default = "v1.2.0"
+}
 
+# Data source to fetch the Gateway API CRD YAML
+data "http" "gateway_api_crd" {
+  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/${var.gateway_api_crd_version}/experimental-install.yaml"
+}
+
+# Resource to apply the Gateway API CRDs
+resource "kubectl_manifest" "gateway_api_crds" {
+  yaml_body = data.http.gateway_api_crd.response_body 
+}
+
+# Install Cilium using Helm
 resource "helm_release" "cilium" {
   name              = "cilium"
   repository        = "https://helm.cilium.io/"
@@ -38,6 +53,7 @@ resource "helm_release" "cilium" {
   values            = [file("${var.manifests_dir}/values.yaml")]
 }
 
+# Apply all the other Cilium manifests, excluding values.yaml
 data "kubectl_path_documents" "docs" {
     pattern = "${var.manifests_dir}/[a-h]*.yaml"
 }
